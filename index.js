8c78170d5a4aa95c1f6b06906e74ec53e770664d
Carlos Villuendas <carlosvillu@gmail.com>
2015-07-27 12:22:21 +0200

    Updates

---
+++ index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './index.scss'
+import i18n, {CHANGE_TRANSLATION_EVENT, rosetta} from '../src';
+import polyglot from '../src/adapters/polyglot';
+
+const spanish = {
+  'HOLA': 'hola',
+  'MUNDO': 'mundo!',
+  'ES': 'Español',
+  'EN': 'Ingles'
+};
+
+const english = {
+  'HOLA': 'hello',
+  'MUNDO': 'world!',
+  'ES': 'Spanish',
+  'EN': 'English'
+};
+
+i18n.adapter = polyglot;
+// Init the app with spanish
+i18n.translations = spanish;
+
+
+class Item extends React.Component {
+  render() {
+    return <li>{i18n.t(this.props.text)}</li>;
+  }
+}
+
+@rosetta
+class List extends React.Component {
+  changeLanguage(language) {
+    i18n.translations = language === 'en' ? english : spanish;
+  }
+  render() {
+    return (
+        <div>
+          <ul>
+            {
+              ["HOLA", "MUNDO"].map((text, index) => {
+                return (<Item key={index} text={text} />);
+              })
+            }
+          </ul>
+          <button onClick={this.changeLanguage.bind(this, 'es')}>{i18n.t('ES')}</button>
+          <button onClick={this.changeLanguage.bind(this, 'en')}>{i18n.t('EN')}</button>
+        </div>
+    )
+  }
+}
+
+React.render(<List />, document.getElementById('list-container'));
