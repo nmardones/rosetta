9c6e04e028beccddbad74a00d9ee8dd55c6e7d71
Carlos Villuendas <carlosvillu@gmail.com>
2015-07-31 18:43:31 +0200

    Fix multirender bug

--- src/decorator.js
+++ src/decorator.js
@@ -2,16 +2,12 @@ import React from 'react';
 import i18n, {CHANGE_TRANSLATION_EVENT} from './';
 
 export const rosetta = (Target) => {
-  return class extends Target {
+  return class Rosetta extends Target {
     constructor() {
       super();
       i18n.on(CHANGE_TRANSLATION_EVENT, () => {
         this.forceUpdate();
       });
-    }
-
-    render() {
-      return (<Target {...this.props} />);
     }
   }
 };
